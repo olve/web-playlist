322f3bddee1b582cecff3ad42766cc948f2f5df2
Olve <oisevb@gmail.com>
2016-09-15 01:02:38 +0200

    remove postcss deps

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,6 @@ module.exports = {
             loaders: [
                 'style-loader',
                 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
-                'postcss-loader'
             ]
         }, {
             loader: 'worker',
